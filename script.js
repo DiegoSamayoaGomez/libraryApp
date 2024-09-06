64b0c2e823d891171f10aa0b4854eced7f5bdfdb
DiegoSamayoaGomez <samayoasg2014@gmail.com>
2024-09-06 16:17:20 -0600

    Show each book stored

--- script.js
+++ script.js
@@ -13,14 +13,28 @@ function Book(title, author, pages, read) {
 }
 
 //Take user input and store the new book object into an array
-function addBookToLibrary() {
-    let title = prompt("Enter book title");
+function addBookToLibrary(title, author, page, read) {
+    /*
+        let title = prompt("Enter book title");
     let author = prompt("Enter book author");
     let page = prompt("Enter number of pages of the book");
     let read = prompt("Have you finished the book?");
-
+    */
     const addBook = new Book(title, author, page, read);
     myLibrary.push(addBook);
-    console.log(addBook.info());
 
 }
+
+addBookToLibrary("book1", "author1", 123, "no");
+addBookToLibrary("book2", "author2", 123, "yes");
+addBookToLibrary("book3", "author2", 123, "no");
+
+
+//Loop through the array and display each book
+function showBook(myArr) {
+    for (let index = 0; index < myArr.length; index++) {
+        console.table(myArr[index]);
+    }
+}
+
+showBook(myLibrary);
