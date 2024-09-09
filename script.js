11a0b9efeda810858308c0b6c6aa7511b2ed0359
DiegoSamayoaGomez <samayoasg2014@gmail.com>
2024-09-09 13:46:37 -0600

    Show cards equals to the number of objects stored in the array

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+//Convert HTML elements into DOM elements
+const addNewBookBtn = document.querySelector(".newCard");
+const libraryElement = document.querySelector(".library");
+
 //Store of book objects
 const myLibrary = [];
 
@@ -14,27 +18,53 @@ function Book(title, author, pages, read) {
 
 //Take user input and store the new book object into an array
 function addBookToLibrary(title, author, page, read) {
-    /*
-        let title = prompt("Enter book title");
-    let author = prompt("Enter book author");
-    let page = prompt("Enter number of pages of the book");
-    let read = prompt("Have you finished the book?");
-    */
     const addBook = new Book(title, author, page, read);
     myLibrary.push(addBook);
 
 }
 
+//Examples
 addBookToLibrary("book1", "author1", 123, "no");
 addBookToLibrary("book2", "author2", 123, "yes");
 addBookToLibrary("book3", "author2", 123, "no");
-
+addBookToLibrary("book3", "author2", 123, "no");
 
 //Loop through the array and display each book
 function showBook(myArr) {
-    for (let index = 0; index < myArr.length; index++) {
-        console.table(myArr[index]);
+    myArr.forEach((books) => {
+        console.log(books.title);
+        console.log(books.author);
+        console.log(books.pages);
+        console.log(books.read);
+        createCard();
+    });
+    /*
+    OLD VERSION
+     for (let index = 0; index < myArr.length; index++) {
+        console.log("------")
+        console.log(myArr[index].title);  
     }
+    */
+
 }
 
 showBook(myLibrary);
+
+//Create a card element which will use the information of the Book Object
+//title, author, page, read
+function createCard() {
+    const cardElement = document.createElement("div");
+    cardElement.classList.add("card");
+    libraryElement.appendChild(cardElement);
+
+}
+
+addNewBookBtn.addEventListener("click", () => {
+    alert("uwu");
+});
+/*
+
+const btn = document.querySelector("#btn");
+btn.addEventListener("click", () => {
+  alert("Hello World");
+});*/
