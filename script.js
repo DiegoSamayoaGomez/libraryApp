4d505892467a2622fe699c00f10816d51b312823
DiegoSamayoaGomez <samayoasg2014@gmail.com>
2024-09-09 14:14:01 -0600

    Populate card with information stored in the array of objects

--- script.js
+++ script.js
@@ -26,17 +26,18 @@ function addBookToLibrary(title, author, page, read) {
 //Examples
 addBookToLibrary("book1", "author1", 123, "no");
 addBookToLibrary("book2", "author2", 123, "yes");
-addBookToLibrary("book3", "author2", 123, "no");
-addBookToLibrary("book3", "author2", 123, "no");
+addBookToLibrary("book3", "author3", 123, "no");
+addBookToLibrary("book4", "author4", 123, "Yes");
 
 //Loop through the array and display each book
 function showBook(myArr) {
     myArr.forEach((books) => {
-        console.log(books.title);
-        console.log(books.author);
-        console.log(books.pages);
-        console.log(books.read);
-        createCard();
+        let title = books.title;
+        let author = books.author;
+        let pages = books.pages;
+        let readStatus = books.read;
+        createCard(title, author, pages, readStatus);
+
     });
     /*
     OLD VERSION
@@ -51,11 +52,35 @@ function showBook(myArr) {
 showBook(myLibrary);
 
 //Create a card element which will use the information of the Book Object
-//title, author, page, read
-function createCard() {
+function createCard(title, author, page, read) {
+    //Create a Card element
     const cardElement = document.createElement("div");
     cardElement.classList.add("card");
     libraryElement.appendChild(cardElement);
+
+    //Create HTML elements that show the book information in individual cards
+    const titleCard = document.createElement("h3");
+    titleCard.classList.add("titleCard");
+    cardElement.appendChild(titleCard);
+    titleCard.textContent = title;
+
+    const authorCard = document.createElement("h4");
+    authorCard.classList.add("authorCard");
+    cardElement.appendChild(authorCard);
+    authorCard.textContent = author;
+    
+    const pagesCard = document.createElement("p");
+    pagesCard.classList.add("pagesCard");
+    cardElement.appendChild(pagesCard);
+    pagesCard.textContent = "pages: " + page;
+    
+    const statusReadCard = document.createElement("p");
+    statusReadCard.classList.add("statusReadCard");
+    cardElement.appendChild(statusReadCard);
+    statusReadCard.textContent = read;
+    
+
+
 
 }
 
